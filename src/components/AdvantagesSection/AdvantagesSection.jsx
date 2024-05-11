b75065da63545761ee0edd3fe2868b98b75edae5
Svitlana Humen <138813162+SvitlanaHu@users.noreply.github.com>
2024-05-12 02:14:11 +0300

    Update AdvantagesSection.jsx

--- src/components/AdvantagesSection/AdvantagesSection.jsx
+++ src/components/AdvantagesSection/AdvantagesSection.jsx
@@ -6,13 +6,13 @@ const AdvantagesSection = () => {
       <div className={styles.advantages}>
         <div className={styles.customersBox}>
           <div className={styles.customersContainer}>
-            <img className={styles.customersImg} src="/public/images/Male3.png" alt="Male3" />
-            <img className={styles.customersImg2} src="/public/images/Male2.png" alt="Male2" />
-            <img className={styles.customersImg3} src="/public/images/Male1.png" alt="Male1" />
+            <img className={styles.customersImg} src="/images/Male3.png" alt="Male3" />
+            <img className={styles.customersImg2} src="/images/Male2.png" alt="Male2" />
+            <img className={styles.customersImg3} src="/images/Male1.png" alt="Male1" />
             <p className={styles.customers}>Our <span className={styles.customersHappy}>happy</span> customers</p>
           </div>          
         </div>
-        
+
         <div className={styles.advantageBox}>
           <div className={styles.advantageContainer}>
             <div className={`${styles.base} ${styles.drive}`}>
