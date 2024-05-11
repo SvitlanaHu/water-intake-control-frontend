88852fb0d532182594da4a90eaba5ace45064e17
Svitlana Humen <138813162+SvitlanaHu@users.noreply.github.com>
2024-05-11 19:46:12 +0300

    add AdvantagesSection

--- src/components/AdvantagesSection/AdvantagesSection.jsx
+++ src/components/AdvantagesSection/AdvantagesSection.jsx
@@ -1,6 +1,19 @@
-// import styles from "./AdvantagesSection.module.css";
+import styles from "./AdvantagesSection.module.css";
 
 const AdvantagesSection = () => {
-  // ваш код
+  return (
+    <div className={styles.advantagesSection}>
+      <div className={styles.advantages}>
+        <div className={styles.customersBox}>
+          <p className={styles.customers}>Our <span className={styles.customersHappy}>happy</span> customers</p>
+        </div>
+        <div className={styles.advantageBox}>
+          <p className={`${styles.base} ${styles.drive}`}>Habit drive</p>
+          <p className={`${styles.statistics} ${styles.base}`}>View statistics</p>        
+          <p className={`${styles.base} ${styles.setting}`}>Personal rate setting</p>
+        </div>
+      </div>
+    </div>
+  );
 };
 export default AdvantagesSection;
