9f6263e84ff502736d98fff67d69b6b9e42208d0
Svitlana Humen <138813162+SvitlanaHu@users.noreply.github.com>
2024-05-12 00:30:02 +0300

    add styles for advantagesSection

--- src/components/AdvantagesSection/AdvantagesSection.jsx
+++ src/components/AdvantagesSection/AdvantagesSection.jsx
@@ -5,7 +5,13 @@ const AdvantagesSection = () => {
     <div className={styles.advantagesSection}>
       <div className={styles.advantages}>
         <div className={styles.customersBox}>
-          <p className={styles.customers}>Our <span className={styles.customersHappy}>happy</span> customers</p>
+          <div className={styles.customersContainer}>
+            <img className={styles.customersImg} src="/public/images/Male3.png" alt="Male3" />
+            <img className={styles.customersImg2} src="/public/images/Male2.png" alt="Male2" />
+            <img className={styles.customersImg3} src="/public/images/Male1.png" alt="Male1" />
+            <p className={styles.customers}>Our <span className={styles.customersHappy}>happy</span> customers</p>
+          </div>
+          
         </div>
         <div className={styles.advantageBox}>
           <p className={`${styles.base} ${styles.drive}`}>Habit drive</p>
