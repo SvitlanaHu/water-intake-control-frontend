bf58189e796835ed45d39e9067d2aadfa09171c8
Serhii-Kysil <serik.kisel000@gmail.com>
2024-05-12 21:27:31 +0300

    Add markup

--- src/components/WaterProgressBar/WaterProgressBar.jsx
+++ src/components/WaterProgressBar/WaterProgressBar.jsx
@@ -1,6 +1,40 @@
-// import styles from "./WaterProgressBar.module.css";
+import css from "./WaterProgressBar.module.css";
+import Slider from "@mui/material/Slider";
 
 const WaterProgressBar = () => {
-  // ваш код
+  return (
+    <div className={css.progressCont}>
+      <div className={css.textCont}>
+        <strong className={css.title}>Today</strong>
+        <Slider
+          className={css.slider}
+          defaultValue={30}
+          sx={{
+            m: 0,
+            p: 0,
+            // height: 6,
+            color: "#9be1a0",
+            "& .MuiSlider-thumb": {
+              borderRadius: "16px",
+              width: "12px",
+              height: "12px",
+              color: "white",
+              border: "1px solid#9be1a0",
+            },
+
+            "& .MuiSlider-rail": {
+              color: "#F0EFF4",
+              backgroundColor: "#F0EFF4",
+            },
+          }}
+        />
+        <div className={css.percentBar}>
+          <a>0%</a>
+          <a>50%</a>
+          <a>100%</a>
+        </div>
+      </div>
+    </div>
+  );
 };
 export default WaterProgressBar;
--- src/pages/TrackerPage.jsx
+++ src/pages/TrackerPage.jsx
@@ -1,8 +1,12 @@
-import React from 'react';
 import WaterMainInfo from "../components/WaterMainInfo/WaterMainInfo";
 import WaterDetailedInfo from "../components/WaterDetailedInfo/WaterDetailedInfo";
-
+import css from "./TrackerPage.module.css";
 const TrackerPage = () => {
-  // ваш код
+  return (
+    <div className={css.trackerContainer}>
+      <WaterMainInfo />
+      <WaterDetailedInfo />
+    </div>
+  );
 };
 export default TrackerPage;
