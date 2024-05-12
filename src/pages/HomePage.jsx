aab88cacaeaca3346bc501debba7ec7178b0ad20
Svitlana Humen <138813162+SvitlanaHu@users.noreply.github.com>
2024-05-12 14:46:07 +0300

    Update HomePage.jsx

--- src/pages/HomePage.jsx
+++ src/pages/HomePage.jsx
@@ -4,11 +4,13 @@ import AdvantagesSection from "../components/AdvantagesSection/AdvantagesSection
 import WelcomeSection from "../components/WelcomeSection/WelcomeSection";
 
 const styles = {
-  container: {
-    display: "flex",
-    alignItems: "center",
-    justifyContent: "center",
-    flexDirection: "column",
+  '@media screen and (minWidth: 1439px)': {
+    container: {
+      display: "flex",
+      alignItems: "center",
+      justifyContent: "center",
+      flexDirection: "column",
+    }
   },
   // Медіа-запит для розширення екрану 1440px
   '@media screen and (minWidth: 1440px)': {
