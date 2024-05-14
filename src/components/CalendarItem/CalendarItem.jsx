a44de163a0461b77fcc06c2650c843b73d3b0fb5
Serhii-Kysil <serik.kisel000@gmail.com>
2024-05-14 10:34:15 +0300

    Add calendar full adapt

--- src/components/CalendarItem/CalendarItem.jsx
+++ src/components/CalendarItem/CalendarItem.jsx
@@ -1,6 +1,12 @@
-// import styles from "./CalendarItem.module.css";
+/* eslint-disable react/prop-types */
+import css from "./CalendarItem.module.css";
 
-const CalendarItem = () => {
-  // ваш код
+const CalendarItem = ({ data }) => {
+  return (
+    <button type="button" className={css.btn}>
+      <p className={(css.backGround, css.day)}>{data.id}</p>
+      <p className={css.water}>{data.water}</p>
+    </button>
+  );
 };
 export default CalendarItem;
--- src/components/CalendarPagination/CalendarPagination.jsx
+++ src/components/CalendarPagination/CalendarPagination.jsx
@@ -1,6 +1,31 @@
-// import styles from "./CalendarPagination.module.css";
+import css from "./CalendarPagination.module.css";
 
 const CalendarPagination = () => {
-  // ваш код
+  const month = "April";
+  const year = "2024";
+
+  return (
+    <div className={css.paginationBlock}>
+      <button className={css.btn} type="button">
+        <svg className={css.svg}>
+          <use
+            className={css.icon}
+            href="../../../public/symbol.svg#icon-chevron-left"
+          ></use>
+        </svg>
+      </button>
+      <p className={css.text}>
+        {month},{year}
+      </p>
+      <button className={css.btn} type="button">
+        <svg className={css.svg}>
+          <use
+            className={css.icon}
+            href="../../../public/symbol.svg#icon-chevron-right"
+          ></use>
+        </svg>
+      </button>
+    </div>
+  );
 };
 export default CalendarPagination;
