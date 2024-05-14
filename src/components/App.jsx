08dfc6eb06facf58d290f4b40a15d894e96582f7
Serhii-Kysil <serik.kisel000@gmail.com>
2024-05-14 21:03:36 +0300

    Add TrackerPage Folder

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -4,23 +4,23 @@ import SharedLayout from './SharedLayout/SharedLayout';
 import HomePage from '../pages/HomePage/HomePage';
 import SignInPage from '../pages/SignInPage';
 import SignUpPage from '../pages/SignUpPage';
-import TrackerPage from '../pages/TrackerPage';
-import Statistics from "../pages/Statistics";
+import TrackerPage from '../pages/TrackerPage/TrackerPage';
+import Statistics from '../pages/Statistics';
 // import { BrowserRouter } from 'react-router-dom';
 
 const App = () => {
-    return (
-        <SharedLayout>
-            <Routes>
-                <Route path="/" element={<HomePage />} />
-                <Route path="/signin" element={<SignInPage />} />
-                <Route path="/signup" element={<SignUpPage />} />
-                <Route path="/tracker" element={<TrackerPage />} />
-                <Route path="/statistics" element={<Statistics />} />
-                {/* Додайте інші маршрути, які вам потрібні */}
-            </Routes>
-        </SharedLayout>
-    );
+  return (
+    <SharedLayout>
+      <Routes>
+        <Route path="/" element={<HomePage />} />
+        <Route path="/signin" element={<SignInPage />} />
+        <Route path="/signup" element={<SignUpPage />} />
+        <Route path="/tracker" element={<TrackerPage />} />
+        <Route path="/statistics" element={<Statistics />} />
+        {/* Додайте інші маршрути, які вам потрібні */}
+      </Routes>
+    </SharedLayout>
+  );
 };
 
 export default App;
