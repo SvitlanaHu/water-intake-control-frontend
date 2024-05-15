5656e7b0ed9dfcdc01dd1c276f86cabb133628d7
Svitlana Humen <138813162+SvitlanaHu@users.noreply.github.com>
2024-05-15 08:43:11 +0300

    Update App.jsx

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -3,8 +3,8 @@
 import { Route, Routes } from 'react-router-dom';
 import SharedLayout from './SharedLayout/SharedLayout';
 import HomePage from '../pages/HomePage/HomePage';
-import SignInPage from '../pages/SignInPage';
-import SignUpPage from '../pages/SignUpPage';
+import SignInPage from '../pages/AuthPages/SignInPage';
+import SignUpPage from '../pages/AuthPages/SignUpPage';
 import TrackerPage from '../pages/TrackerPage/TrackerPage';
 import Statistics from '../pages/Statistics';
 
