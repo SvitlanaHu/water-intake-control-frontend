d60db29d52ed14ee72b2f37d9e25b428bfda8783
Serhii-Kysil <serik.kisel000@gmail.com>
2024-05-17 12:51:11 +0300

    Add day, Month

--- src/components/ChooseDate/ChooseDate.jsx
+++ src/components/ChooseDate/ChooseDate.jsx
@@ -1,9 +1,15 @@
 import { useSelector } from 'react-redux';
 import css from './ChooseDate.module.css';
+import dayjs from 'dayjs';
 
 const ChooseDate = () => {
   const selectedDate = useSelector(state => state.calendar.selectedDate);
-  return <p className={css.text}>{selectedDate}</p>;
+  const currentDate = dayjs().format('YYYY-MM-DD');
+
+  const formattedDate = dayjs(selectedDate).format('D, MMMM');
+  const isToday = selectedDate === currentDate;
+
+  return <p className={css.text}>{isToday ? 'Today' : formattedDate}</p>;
 };
 
 export default ChooseDate;
