67c57ef79b7a0df8f5d8cb0444f8f478874c56f4
Serhii-Kysil <serik.kisel000@gmail.com>
2024-05-20 01:40:10 +0300

    AddWaterDetailed

--- src/components/DailyInfo/DailyInfo.jsx
+++ src/components/DailyInfo/DailyInfo.jsx
@@ -1,14 +1,26 @@
-import css from "./DailyInfo.module.css";
-import ChooseDate from "../ChooseDate/ChooseDate";
-import AddWaterDetailedBtn from "../AddWaterDetailedBtn/AddWaterDetailedBtn";
-import WaterList from "../WaterList/WaterList";
+import css from './DailyInfo.module.css';
+import ChooseDate from '../ChooseDate/ChooseDate';
+import AddWaterDetailedBtn from '../AddWaterDetailedBtn/AddWaterDetailedBtn';
+import WaterList from '../WaterList/WaterList';
+import WaterModal from '../WaterModal/WaterModal';
+import { useState } from 'react';
 
 const DailyInfo = () => {
+  const [isModalOpen, setIsModalOpen] = useState(false);
+
+  const handleOpenModal = () => {
+    setIsModalOpen(true);
+  };
   return (
     <div className={css.dailyCont}>
       <div className={css.dateAdd}>
         <ChooseDate />
-        <AddWaterDetailedBtn />
+        <AddWaterDetailedBtn handleOpenModal={handleOpenModal} />
+        <WaterModal
+          operationType="add"
+          isModalOpen={isModalOpen}
+          setIsModalOpen={setIsModalOpen}
+        />
       </div>
       <div className={css.scrollDiv}>
         <WaterList />
